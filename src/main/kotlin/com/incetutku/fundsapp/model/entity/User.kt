94aacf56144218c6d54d3c015b202150c4a94ba6
Tutku Ince <tutku.ince@outlook.com>
2025-04-09 19:24:22 +0300

    user entity

---
+++ src/main/kotlin/com/incetutku/fundsapp/model/entity/User.kt
@@ -0,0 +1,31 @@
+package com.incetutku.fundsapp.model.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.util.*
+
+@Entity
+@Table(name = "users")
+class User {
+
+    @Id
+    @GeneratedValue
+    var id: Long? = null
+    var name: String = ""
+    var surname: String = ""
+    var email: String = ""
+    var password: String = ""
+    var age: Int = 0
+    var defaultAccountId: Long? = null
+
+    @CreationTimestamp
+    val createdAt: Date? = null
+
+    @UpdateTimestamp
+    val updatedAt: Date? = null
+
+}
